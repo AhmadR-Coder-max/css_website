e84362a6b47c4c4dcdffe2322fcbbad7fca19834
AhmadR-Coder-max <upworka786@gmail.com>
2025-11-11 10:55:00 +0500

    Add basic sections

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about_us; end
+
+  def events; end
+
+  def team; end
+
+  def announcements; end
+
+  def contact; end
+end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "vue" # @3.5.21
+pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.turbo.min.js"
+
